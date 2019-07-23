62ce40f1fcde5b5dc27a4ff082a031feb0b12977
zengming <zengming00@qq.com>
2019-07-23 08:58:31 +0800

    quickjs 2019-07-21

--- build-qjs.bat
+++ build-qjs.bat
@@ -1,6 +1,6 @@
 rem TDM-GCC
-qjsc.exe -c -o repl.c -m quickjs-2019-07-09/repl.js
+qjsc.exe -c -o repl.c -m quickjs-src/repl.js
 gcc -DCONFIG_VERSION=\"0.0.1\" -c repl.c -o repl.o
-gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-2019-07-09/qjs.c -o qjs.o
+gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-src/qjs.c -o qjs.o
 gcc -DCONFIG_VERSION=\"0.0.1\" -o qjs.exe repl.o quickjs.o libregexp.o libunicode.o cutils.o quickjs-libc.o qjs.o
 
--- build-qjsc.bat
+++ build-qjsc.bat
@@ -1,9 +1,9 @@
 rem TDM-GCC
-gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-2019-07-09/quickjs.c -o quickjs.o
-gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-2019-07-09/libregexp.c -o libregexp.o
-gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-2019-07-09/libunicode.c -o libunicode.o
-gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-2019-07-09/cutils.c -o cutils.o
-gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-2019-07-09/quickjs-libc.c -o quickjs-libc.o
+gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-src/quickjs.c -o quickjs.o
+gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-src/libregexp.c -o libregexp.o
+gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-src/libunicode.c -o libunicode.o
+gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-src/cutils.c -o cutils.o
+gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-src/quickjs-libc.c -o quickjs-libc.o
 
-gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-2019-07-09/qjsc.c -o qjsc.o
+gcc -DCONFIG_VERSION=\"0.0.1\" -c quickjs-src/qjsc.c -o qjsc.o
 gcc -DCONFIG_VERSION=\"0.0.1\" -o qjsc.exe  quickjs.o libregexp.o libunicode.o cutils.o quickjs-libc.o qjsc.o
